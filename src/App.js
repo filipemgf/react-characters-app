26160f3b638618e2eaec1e83065c1c111bf289e8
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-06-22 16:05:22 +0200

    (useEffect) display list of characters from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const baseURL = "https://ih-crud-api.herokuapp.com";
+
+  const [charactersArr, setCharactersArr] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${baseURL}/characters`)
+      .then( response => {
+        setCharactersArr(response.data);
+      })
+      .catch( e => console.log(e))
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Charates App</h1>
+
+      {
+        charactersArr === null 
+        ? "loading...."
+        : <h2>Number of characters in the API: {charactersArr.length}</h2>
+      }
+
+      {charactersArr && charactersArr.map((characterObj) => {
+        return(
+          <div key={characterObj.id} className="character box">
+            Name: {characterObj.name} <br />
+            Weapon: {characterObj.weapon}
+          </div>
+        )
+      })}
+
+
     </div>
   );
 }
